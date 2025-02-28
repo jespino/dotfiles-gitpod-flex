2ef4a4d02dc94eb0eb176132663e2a807f9dabaa
Wouter Verlaek <wverlaek@hotmail.nl>
2025-02-28 15:09:08 +0100

    Fix themes

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ if [[ -d "$HOME/dotfiles" ]]; then
     # In gitpod, dotfiles are stored in this directory
     pushd "$HOME/dotfiles"
         mv .zshrc "$HOME/.zshrc"
-        mv .oh-my-zsh/themes "$HOME/.oh-my-zsh/themes"
+        cp -r .oh-my-zsh/themes/* "$HOME/.oh-my-zsh/themes/"
     popd
 fi
 
